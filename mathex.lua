4a29cf6ecaf58c1203df628753b5965a630d658f
J.C. Moyer <jmoyer1992@gmail.com>
2013-04-27 00:12:34 -0400

    Clamping function

--- mathex.lua
+++ mathex.lua
@@ -4,4 +4,10 @@ function mathex.lerp(v0, v1, t)
   return v0 + (v1 - v0) * t
 end
 
+function mathex.clamp(x, min, max)
+  if x < min then return min end
+  if x > max then return max end
+  return x
+end
+
 return mathex
