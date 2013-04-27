121a0b812782aa193844c2eeedd06635abdd303e
J.C. Moyer <jmoyer1992@gmail.com>
2013-04-27 00:25:35 -0400

    Basic camera object

---
+++ camera.lua
@@ -0,0 +1,27 @@
+local mathex = require('mathex')
+local camera = {}
+
+function camera.new(width, height)
+  local instance = {
+    x = 0,
+    y = 0,
+    w = width,
+    h = height
+  }
+  return setmetatable(instance, { __index = camera })
+end
+
+function camera:center(x, y)
+  self.x = self.w / 2 - x
+  self.y = self.h / 2 - y
+end
+
+function camera:panCenter(x, y, dt)
+  local cx = self.x
+  local cy = self.y
+  self:center(x, y)
+  self.x = mathex.lerp(cx, self.x, dt * 2)
+  self.y = mathex.lerp(cy, self.y, dt * 2)
+end
+
+return camera
