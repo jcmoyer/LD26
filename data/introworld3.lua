76a1301b2f1868daa070bc98e0d779d912bec466
J.C. Moyer <jmoyer1992@gmail.com>
2013-04-27 08:29:40 -0400

    Add puzzle room for introworld3

--- data/introworld3.lua
+++ data/introworld3.lua
@@ -3,18 +3,43 @@ local data = {}
 data.background = { 215, 215, 215 }
 
 data.lines = {
-  0,   300,
-  2000,300
+  0, 300,
+  100, 300,
+  200, 250,
+  300, 250,
+  400, 200,
+  500, 200,
+  600, 150,
+  700, 150,
+  800, 100,
+  900, 100,
+  1000, 50,
+  1100, 50,
+  1200, 0,
+  1300, 0
 }
 
 data.portals = {
-  { x = 1400, destination = 'data.introworld', dx = 50 }
+  { x = 650 , destination = 'data.iw3r1'      , dx = 650 },
+  { x = 1250, destination = 'data.introworld3', dx = 50 }
+}
+
+data.regions = {
+  { name = 'question', x = 650, w = 20 }
 }
 
 data.triggers = {}
 
 function data.triggers.onEnter(context)
   context.showMessage('don\'t you know it\s dangerous?', 10)
+end
+
+local r_question_triggered = false
+function data.triggers.onEnterRegion(context, r)
+  if (r.name == 'question' and not question_triggered) then
+    context.showMessage('just go away', 10)
+    r_question_triggered = true
+  end
 end
 
 return data
---
+++ data/iw3r1.lua
@@ -0,0 +1,71 @@
+local data = {}
+
+data.lines = {
+  0, 300,
+  -- 50: Switch 1
+  100, 300,
+  200, 325,
+  300, 325,
+  400, 300,
+  -- 450: Switch 2
+  500, 300,
+  600, 325,
+  650, 325, -- Spawn player HERE
+  700, 325,
+  800, 300,
+  -- 850: Switch 3
+  900, 300,
+  1000, 325,
+  1100, 325,
+  1200, 300,
+  -- 1250: Switch 4
+  1300, 300
+}
+
+data.portals = {
+  { x = 650 , destination = 'data.introworld3', dx = 650  },
+  { x = 250 , destination = 'data.iw3r1'      , dx = 1050 },
+  { x = 1050, destination = 'data.iw3r1'      , dx = 250  }
+}
+
+data.switches = {
+  { name = 'sw1', x = 50   , ud = 1 },
+  { name = 'sw2', x = 450  , ud = 2 },
+  { name = 'sw3', x = 850  , ud = 3 },
+  { name = 'sw4', x = 1250 , ud = 4 }
+}
+
+data.triggers = {}
+
+function setAllSwitches(context, status)
+  for _,v in ipairs(data.switches) do
+    context.setSwitchStatus(v.name, status)
+  end
+end
+
+function data.triggers.onEnter(context)
+  context.showMessage('no seriously', 5)
+  if context.getVar('introworld3.continue') then
+    setAllSwitches(context, true)
+  end
+end
+
+-- Correct order is 3-1-4-2
+local nextSwitch  = 3
+local switchIndex = 1
+local switchOrder = { 3, 1, 4, 2 }
+function data.triggers.onSwitchChanged(context, s)
+  if switchOrder[switchIndex] == s.ud then
+    switchIndex = switchIndex + 1
+    if switchIndex > #switchOrder then
+      context.showMessage('stop that', 10)
+      context.setVar('introworld3.continue', true)
+    end
+  else
+    -- wrong choice
+    setAllSwitches(context, false)
+    switchIndex = 1
+  end
+end
+
+return data
