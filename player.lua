f52c250a17fd87717c956d6f19e0ae8018806d04
J.C. Moyer <jmoyer1992@gmail.com>
2013-04-26 23:54:18 -0400

    Add width and height fields

--- player.lua
+++ player.lua
@@ -3,7 +3,9 @@ local player = {}
 function player.new()
   local instance = {
     x = 0,
-    y = 0
+    y = 0,
+    w = 32,
+    h = 64
   }
   return setmetatable(instance, { __index = player })
 end
@@ -11,7 +13,7 @@ end
 function player:draw()
   local g = love.graphics
   g.setColor(255, 255, 255)
-  g.rectangle('fill', self.x, self.y, 32, 64)
+  g.rectangle('fill', self.x, self.y - self.h, self.w, self.h)
 end
 
 return player
