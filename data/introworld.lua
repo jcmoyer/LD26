4c5ddaf9800703983e32f1a890432cca8845fe73
J.C. Moyer <jmoyer1992@gmail.com>
2013-04-27 00:07:54 -0400

    World test data

---
+++ data/introworld.lua
@@ -0,0 +1,11 @@
+local data = {}
+
+data.lines = {
+  0, 300,
+  200, 350,
+  400, 290,
+  500, 350
+
+}
+
+return data
--- main.lua
+++ main.lua
@@ -1,8 +1,21 @@
+local player = require('player')
+local world = require('world')
+
+local p = player.new()
+local w = world.new('data.introworld')
+
 function love.load()
 end
 
 function love.draw()
+  local g = love.graphics
+  g.clear()
+
+  w:draw()
+  p:draw()
 end
 
 function love.update(dt)
+  p.x = p.x + 1
+  p.y = w:y(p.x)
 end
