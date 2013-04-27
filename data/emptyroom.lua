95d0e410b9d0fd9231530761097e48db793adc80
J.C. Moyer <jmoyer1992@gmail.com>
2013-04-27 06:48:02 -0400

    Add a switch to emptyroom

--- data/emptyroom.lua
+++ data/emptyroom.lua
@@ -11,4 +11,33 @@ data.portals = {
   { x = 50, destination = 'data.introworld', dx = 984 }
 }
 
+data.regions = {
+  { name = 'switch', x = 350, w = 16 }
+}
+
+data.triggers = {}
+
+function data.triggers.onEnterRegion(context, r)
+  if r.name == 'switch' then
+    context.setVar('emptyroom.switch', true)
+  end
+end
+
+function data.triggers.onDraw(context)
+  local g = love.graphics
+  local switchReg = data.regions[1]
+  local status    = context.getVar('emptyroom.switch')
+  if status then
+    local hw = switchReg.w / 2
+    local y  = context.y(switchReg.x)
+    g.setColor(context.preferredColor())
+    g.rectangle('fill', switchReg.x - hw, y - 2, switchReg.w, 2)
+  else
+    local hw = switchReg.w / 2
+    local y  = context.y(switchReg.x)
+    g.setColor(context.preferredColor())
+    g.rectangle('fill', switchReg.x - hw, y - 8, switchReg.w, 8)
+  end
+end
+
 return data
