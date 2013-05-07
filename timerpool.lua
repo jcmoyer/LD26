8f4c14a59ca002ff0002dc9c9786d2bf38d6953d
J.C. Moyer <jmoyer1992@gmail.com>
2013-05-07 05:42:43 -0400

    Timerpool object to manage timers

---
+++ timerpool.lua
@@ -0,0 +1,40 @@
+local timerpool = {}
+local timers = {}
+
+function timerpool.start(duration, callback)
+  local remaining = duration
+  local timer = {}
+  function timer.getCallback()
+    return callback
+  end
+  function timer.getRemaining()
+    return remaining
+  end
+  function timer.getDuration()
+    return duration
+  end
+  function timer.update(dt)
+    remaining = remaining - dt
+  end
+  function timer.finished()
+    return remaining <= 0
+  end
+  
+  timers[#timers + 1] = timer
+  
+  return timer
+end
+
+function timerpool.update(dt)
+  for i = #timers, 1, -1 do
+    local t = timers[i]
+    t.update(dt)
+    if t.finished() then
+      local f = t.getCallback()
+      if f then f() end
+      table.remove(timers, i)
+    end
+  end
+end
+
+return timerpool
