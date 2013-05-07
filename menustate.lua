a9c1d4fe9fac0a15fe412eff45de828d40066179
J.C. Moyer <jmoyer1992@gmail.com>
2013-05-07 04:59:04 -0400

    Display a random scrolling world on the menu screen

--- menustate.lua
+++ menustate.lua
@@ -1,25 +1,34 @@
 local menu = require('menu')
 local sound = require('sound')
+local world = require('world')
+local camera = require('camera')
+local mathex = require('mathex')
+local gamecontext = require('gamecontext')
 local playstate = require('playstate')
 local gamestate = require('gamestate')
 local menustate = gamestate.new()
 
+local datafiles = love.filesystem.enumerate('data')
 local headerfont = love.graphics.newFont(48)
 local font = love.graphics.newFont(18)
 
 function menustate.new()
-  local instance = {
-    menu = menu.new(font)
-  }
-  instance.menu.x = love.graphics.getWidth() / 2
-  instance.menu.y = love.graphics.getHeight() / 2
+  local g = love.graphics
+  local instance = setmetatable({}, { __index = menustate })
+  instance.currentworld = nil
+  instance.camera = camera.new(g.getWidth(), g.getHeight())
+  instance.x = 0
+  instance.menu = menu.new(font)
+  instance.menu.x = g.getWidth() / 2
+  instance.menu.y = g.getHeight() / 2
   instance.menu:add('Start', function()
     instance:sm():changeState(playstate.new())
   end)
   instance.menu:add('Exit', function()
     love.event.quit()
   end)
-  return setmetatable(instance, { __index = menustate })
+  instance:setRandomWorld()
+  return instance
 end
 
 function menustate:keypressed(key)
@@ -37,9 +46,39 @@ function menustate:keypressed(key)
   end
 end
 
+function menustate:update(dt)
+  self.camera:update(dt)
+  
+  self.camera:panCenter(self.x, self.currentworld:y(self.x) - 100, dt)
+  self.x = self.x + 50 * dt
+  
+  if self.x > self.currentworld:right() then
+    self:setRandomWorld()
+  end
+end
+
 function menustate:draw()
+  local g = love.graphics
+  g.setBackgroundColor(self.currentworld.background)
+  g.clear()
+  
+  g.translate(self.camera:calculatedX(), self.camera:calculatedY())
+  self.currentworld:draw()
+  
+  -- reverse the translation to draw the overlay
+  g.translate(-self.camera:calculatedX(), -self.camera:calculatedY())
   drawHeader()
   self.menu:draw()
+end
+
+function menustate:setRandomWorld()
+  local lastname = currentworld and currentworld.name or nil
+  local emptycontext = gamecontext.new()
+  repeat
+    self.currentworld = world.new(pickWorldName(), emptycontext)
+  until self.currentworld.name ~= lastname
+  self.x = self.currentworld:left()
+  self.camera:center(self.x, self.currentworld:y(self.x) - 100)
 end
 
 function drawHeader()
@@ -50,6 +89,17 @@ function drawHeader()
   g.setColor(255, 255, 255)
   g.setFont(headerfont)
   g.print(text, love.graphics.getWidth() / 2 - w / 2, love.graphics.getHeight() / 6 - h / 2)
+end
+
+function pickWorldName()
+  local worldnames = {}
+  for i = 1, #datafiles do
+    local _, _, name = datafiles[i]:find('([%a%d_]+)%.lua$')
+    if name then
+      worldnames[#worldnames + 1] = name
+    end
+  end
+  return 'data.' .. worldnames[math.random(#worldnames)]
 end
 
 return menustate
