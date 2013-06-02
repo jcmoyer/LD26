12bca6200f9e642d225590e1c2c20a1634b83ff7
J.C. Moyer <jmoyer1992@gmail.com>
2013-06-02 16:56:21 -0400

    Add button to return to main menu on gameover screen

--- gameoverstate.lua
+++ gameoverstate.lua
@@ -1,3 +1,6 @@
+local uiscene = require('ui.scene')
+local uibutton = require('ui.button')
+
 local gamestate = require('gamestate')
 local gameoverstate = setmetatable({}, { __index = gamestate })
 
@@ -13,9 +16,34 @@ local submessageWidth = gameoverSubFont:getWidth(submessage)
 
 function gameoverstate.new(message)
   local instance = {
-    message = message
+    message = message,
+    ui = uiscene.new()
   }
+  
+  local btnmenu = uibutton.new()
+  btnmenu.text = "Main Menu"
+  btnmenu.w = 100
+  btnmenu.h = 50
+  btnmenu.x = love.graphics.getWidth() - btnmenu.w - 32
+  btnmenu.y = love.graphics.getHeight() - btnmenu.h - 32
+  btnmenu.events.click:add(function()
+      instance:sm():pop()
+    end)
+  instance.ui:addchild(btnmenu)
+  
   return setmetatable(instance, { __index = gameoverstate })
+end
+
+function gameoverstate:mousepressed(x, y, button)
+  self.ui:mousepressed(x, y, button)
+end
+
+function gameoverstate:mousereleased(x, y, button)
+  self.ui:mousereleased(x, y, button)
+end
+
+function gameoverstate:update(dt)
+  self.ui:update(dt)
 end
 
 function gameoverstate:draw()
@@ -33,6 +61,8 @@ function gameoverstate:draw()
     local udsw = gameoverSubFont:getWidth(self.message)
     g.print(self.message, w / 2 - udsw / 2, h / 3)
   end
+  
+  self.ui:draw()
 end
 
 return gameoverstate
