f1308c41a26018cf680c9c43178ec0dcd31f8430
J.C. Moyer <jmoyer1992@gmail.com>
2013-06-08 01:56:52 -0400

    Move love callbacks into core/basicgame.lua

---
+++ core/basicgame.lua
@@ -0,0 +1,41 @@
+local statemachine = require('statemachine')
+local timerpool = require('timerpool')
+
+local basicgame = {}
+
+local sm = statemachine.new()
+
+function basicgame.start(initialstate)
+  sm:push(initialstate)
+
+  function love.load(args)
+    sm:push(initialstate)
+  end
+
+  function love.draw()
+    sm:draw()
+  end
+
+  function love.update(dt)
+    timerpool.update(dt)
+    sm:update(dt)
+  end
+
+  function love.keypressed(key, unicode)
+    sm:keypressed(key, unicode)
+  end
+
+  function love.keyreleased(key, unicode)
+    sm:keyreleased(key)
+  end
+
+  function love.mousepressed(x, y, button)
+    sm:mousepressed(x, y, button)
+  end
+
+  function love.mousereleased(x, y, button)
+    sm:mousereleased(x, y, button)
+  end
+end
+
+return basicgame
--- main.lua
+++ main.lua
@@ -1,34 +1,4 @@
-local statemachine = require('statemachine')
+local basicgame = require('core.basicgame')
 local menustate = require('menustate')
-local timerpool = require('timerpool')
 
-local sm = statemachine.new()
-
-function love.load(args)
-  sm:push(menustate.new())
-end
-
-function love.draw()
-  sm:draw()
-end
-
-function love.update(dt)
-  timerpool.update(dt)
-  sm:update(dt)
-end
-
-function love.keypressed(key, unicode)
-  sm:keypressed(key, unicode)
-end
-
-function love.keyreleased(key, unicode)
-  sm:keyreleased(key)
-end
-
-function love.mousepressed(x, y, button)
-  sm:mousepressed(x, y, button)
-end
-
-function love.mousereleased(x, y, button)
-  sm:mousereleased(x, y, button)
-end
+basicgame.start(menustate.new())
