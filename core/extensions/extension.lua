0744203f02047baa2a1e859a7072cffcb5ec191e
J.C. Moyer <jmoyer1992@gmail.com>
2013-06-23 23:58:25 -0400

    Create reusable extension module

---
+++ core/extensions/extension.lua
@@ -0,0 +1,20 @@
+local extension = {}
+
+-- installs the members of table 'ext' into table 't'
+function extension.install(ext, t)
+  if type(ext) ~= 'table' then
+    error('ext must be a table')
+  end
+  if type(t) ~= 'table' then
+    error('t must be a table')
+  end
+  for k,v in pairs(ext) do
+    if t[k] then
+      error('(extension) duplicate key: "' .. k .. '"')
+    else
+      t[k] = v
+    end
+  end
+end
+
+return extension
