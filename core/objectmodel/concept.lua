300f2a6a16458e1eba2e95425c12953060cebdf3
J.C. Moyer <jmoyer1992@gmail.com>
2013-07-01 03:07:28 -0400

    Preliminary concept support

---
+++ core/objectmodel/concept.lua
@@ -0,0 +1,41 @@
+local concept = {}
+local mt      = { __index = concept }
+
+function concept.new(t)
+  return setmetatable(t, mt)
+end
+
+function concept:isconcept()
+  return getmetatable(self) == mt
+end
+
+function concept:check(t)
+  return pcall(concept.enforce, self, t)
+end
+
+function concept:enforce(t)
+  for k,v in pairs(self) do
+    local tktype = type(t[k])
+    
+    if tktype ~= v.type then
+      error(k .. ' is not supported by this table')
+      
+      -- enforce nested concepts
+      if tktype == 'table' and concept.isconcept(v.concept) then
+        v.concept:enforce(t[k])
+      end
+    end
+  end
+end
+
+concept.constructible = concept.new({
+  new = {
+    type = 'function'
+  }
+})
+
+--
+-- TODO: x = { optional = true }
+--       y = { rule = function(data) return type(data) == 'string' or type(data) == 'number' end }
+
+return concept
