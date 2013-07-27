jcmoyer/LD26

commit cde30f943241d3897c24662df3379e6e5abf8fe2

Explicitly check for true

enemy.lua
@@ -25,7 +25,7 @@ function enemy.new(owner, x, patrol, size, speed)
 end
 
 function enemy:update(dt)
-  if not self.patrol then return end
+  if self.patrol ~= true then return end
   if self.direction == 'right' then
     self.x = self.x + dt * self.speed
     if self.x >= self.patrol.right then

commit 8142f97d784a828942ac1310b5113dc7b6141e85

Explicitly check for nil
This fixes a bug introduced in cde30f94 where patrol tables weren't being
read properly.

enemy.lua
@@ -25,7 +25,7 @@ function enemy.new(owner, x, patrol, size, speed)
 end
 
 function enemy:update(dt)
-  if self.patrol ~= true then return end
+  if self.patrol == nil then return end
   if self.direction == 'right' then
     self.x = self.x + dt * self.speed
     if self.x >= self.patrol.right then